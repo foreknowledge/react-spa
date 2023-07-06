1b71414ec579593d1a82132d766b477d6b0ecab5
foreknowledge <kyj_0129@naver.com>
2023-07-06 21:56:11 +0900

    feat: Handle pop state event

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import RouterContext from '../context/RouterContext';
 
 interface Props {
@@ -9,8 +9,18 @@ const Router = ({ children }: Props) => {
   const [path, setPath] = useState(window.location.pathname);
 
   const changePath = useCallback((path: string) => {
-    window.history.pushState({}, '', path);
+    window.history.pushState({ path }, '', path);
     setPath(path);
+  }, []);
+
+  useEffect(() => {
+    const popStateHandler = (e: PopStateEvent) => {
+      setPath(e.state.path);
+    };
+
+    window.addEventListener('popstate', popStateHandler);
+
+    return () => window.removeEventListener('popstate', popStateHandler);
   }, []);
 
   return (
