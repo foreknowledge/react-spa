4cf2856cdbaa1651c3baa0155414d46b27fbcbed
foreknowledge <kyj_0129@naver.com>
2023-07-06 21:50:50 +0900

    feat: Add root & about page with router

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <h1>about</h1>
+      <button>go main</button>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Root.tsx
@@ -0,0 +1,12 @@
+const Root = () => {
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <h1>root</h1>
+      <button>about</button>
+    </div>
+  );
+};
+
+export default Root;
